dc08a878058822d94abca1645aaab5093fd993c0
zyd <363797271@qq.com>
2020-08-16 18:31:45 +0800

    actions deploy

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <h1>Github Pages Demo</h1>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
